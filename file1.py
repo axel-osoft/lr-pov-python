5c7035da7de6494fcc8e6077a9a2e18c5313ffc7
axel-osoft <75009084+axel-osoft@users.noreply.github.com>
2024-06-21 12:37:37 +0200

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,4 +1,21 @@
 from requests_oauthlib.oauth2_session import OAuth2Session
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/login')
+def login():
+    dynamodb = AWS_SESSION.client('dynamodb')
+
+    username = request.args["username"]
+    password = request.args["password"]
+
+    dynamodb.scan(
+        FilterExpression= "username = " + username + " and password = " + password, # Noncompliant
+        TableName="users",
+        ProjectionExpression="username"
+    )
+
 
 scope = ['https://www.api.example.com/auth/example.data']
 
